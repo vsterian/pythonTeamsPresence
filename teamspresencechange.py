5731c64eda82e3627d5ca4fd33e01dacf55d6de6
Vlad Sterian <vlad.sterian02@gmail.com>
2023-09-12 17:43:41 +0300

    new function for creation of subscription

--- teamspresencechange.py
+++ teamspresencechange.py
@@ -3,6 +3,17 @@ import webbrowser
 import logging
 import msal
 import json
+from azure.identity.aio import ClientSecretCredential
+from msgraph import GraphServiceClient
+from msgraph.generated.subscriptions.subscriptions_request_builder import SubscriptionsRequestBuilder
+import asyncio
+from msgraph import GraphRequestAdapter
+
+from msgraph.generated.models.subscription import Subscription
+
+
+
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,6 +21,8 @@ logging.basicConfig(level=logging.INFO)
 client_id = '4013e8bf-7904-421c-8c8b-5f32441edff6'
 tenant_id = '1f97ae5f-b765-4494-a8a4-5b0955d6673a'
 authority = f'https://login.microsoftonline.com/{tenant_id}'
+client_secret ='5xs8Q~kJUuMVGpKmYib5NfMNA_iYqIcObrIOrapw'
+scopes = ['https://graph.microsoft.com/.default']
 
 # MS Teams user id
 user_id = '1ecbd423-b0cc-4955-925a-589859e8d58b'
@@ -18,7 +31,6 @@ app = msal.PublicClientApplication(
     client_id, authority=authority,
 )
 result = None
-
 
 accounts = app.get_accounts()
 if accounts:
@@ -34,42 +46,51 @@ if not result:
     webbrowser.open(flow["verification_uri"])  # Open the verification_uri in the default web browser
     result = app.acquire_token_by_device_flow(flow)  # By default, it will block until the user completes the flow
 
-if "access_token" in result:
+credential = ClientSecretCredential(
+    tenant_id=tenant_id,
+    client_id=client_id,
+    client_secret=client_secret)
 
+
+request_adapter = GraphRequestAdapter(auth_provider=authority)
+graph_client = GraphServiceClient(credential, scopes)
+
+if "access_token" in result:
     access_token = result['access_token']
     logging.info(f"Access token retrieved: {access_token}")
     
-    headers = {
-        'Authorization': 'Bearer ' + access_token,
-    }
-    
-   #This part down , does not work. It seems like you need to pass some additional certificates for this presence change 
-
     # Define the subscription payload
-    subscription_data = {
-        "changeType": "updated",
-        "notificationUrl": "https://wildteamspresence1.azurewebsites.net/api/presenceNotification?code=umn1zuc1CkeJutJCf1qoK5J0MdfBxx1obJjTM0zC8s80AzFuSxPtEQ==",
-     
-        "includeResourceData": "true",
-        "resource": f"/communications/presences/{user_id}",
-        "expirationDateTime":"2023-09-12T18:23:45.9356913Z",
-      
-    }
+    
+async def create_subscription():
 
-    subscription_url = "https://graph.microsoft.com/v1.0/subscriptions"
+        #subscription_start = SubscriptionsRequestBuilder(request_adapter=request_adapter)
+        subscription_data = Subscription(
+        change_type = "updated",
+        notification_url = "https://wildteamspresence1.azurewebsites.net/api/presenceNotification?code=umn1zuc1CkeJutJCf1qoK5J0MdfBxx1obJjTM0zC8s80AzFuSxPtEQ==",
+        resource =  f"/communications/presences/{user_id}",
+        expiration_date_time = "2023-09-12T15:50:45.9356913Z",
+        client_state = None,
+       
+)
+        
+        try:
+            
+            
+            
+            # Create the subscription using the GraphServiceClient
+            logging.info("Creating the subscription...")
+            
 
-    try:
-        # Create the subscription using the access token
-        logging.info("Creating the subscription...")
-        create_subscription_response = requests.post(subscription_url, headers=headers, json=subscription_data)
-        create_subscription_response.raise_for_status()
-        print("Subscription to presence changes created successfully.")
-    except requests.exceptions.HTTPError as http_err:
-        logging.error(f"HTTP error occurred: {http_err}")
-        logging.error(f"Response content: {create_subscription_response.content}")
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-else:
-    print(result.get("error"))
-    print(result.get("error_description"))
-    print(result.get("correlation_id"))  # You might need this when reporting a bug.
+            created_subscription = await graph_client.subscriptions.post(body= subscription_data)
+            print("Subscription to presence changes created successfully.")
+        except requests.exceptions.HTTPError as http_err:
+            logging.error(f"HTTP error occurred: {http_err}")
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}")
+        else:
+            print(result.get("error"))
+            print(result.get("error_description"))
+            print(result.get("correlation_id"))  # You might need this when reporting a bug.
+if __name__=="__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(create_subscription())
