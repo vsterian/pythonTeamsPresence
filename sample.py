619d1194a9bc3287cfcc4494b02997aac113f95e
Vlad Sterian <vlad.sterian02@gmail.com>
2023-09-14 14:58:28 +0300

    working version.subscription renewal

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import threading
+import logging  # Import the logging module
 from azure.identity import InteractiveBrowserCredential
 from datetime import datetime, timedelta
 
@@ -15,12 +16,16 @@ user_id = '6e65cc58-12fb-4c0d-befa-147fe89270c9'
 # Create an instance of InteractiveBrowserCredential
 credential = InteractiveBrowserCredential(client_id=client_id, tenant_id=tenant_id, redirect_uri='http://localhost:8000')
 
-expiration_time = datetime.utcnow() + timedelta(minutes=58)
-expiration_time_str = expiration_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 def renew_subscription():
     # Get a token for the Graph API scope
     token = credential.get_token('Presence.Read.All')
+    expiration_time = datetime.utcnow() + timedelta(minutes=9)
+    expiration_time_str = expiration_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     if token:
         # Call a protected API with the access token.
@@ -29,9 +34,9 @@ def renew_subscription():
         # Create a subscription to presence changes
         subscription_data = {
             "changeType": "updated",
-            "notificationUrl": "https://wildteamspresence1.azurewebsites.net/api/presenceNotification?code=umn1zuc1CkeJutJCf1qoK5J0MdfBxx1obJjTM0zC8s80AzFuSxPtEQ==",
+            "notificationUrl": "https://wildteamspresence1.azurewebsites.net/api/webhookMSteamsPresenceChange?code=B6v6velmdkfZteTEe2R50tMbiexjlfYG7FRFH9wQuFPSAzFuA9NPgQ==",
             "resource": f"/communications/presences/{user_id}",
-            "expirationDateTime":expiration_time_str,
+            "expirationDateTime": expiration_time_str,
         }
 
         response = requests.post(
@@ -40,18 +45,29 @@ def renew_subscription():
             json=subscription_data
         )
 
+        # Log the JSON response
+        logging.info(f"Response JSON: {response.text}")
+
         if response.status_code == 201:
             print("Subscription renewed successfully.")
         elif response.status_code == 409:
-            print("A subscription already exists. It must be deleted before another can be made.")
-
+            # Extract and print the subscriptionId from the response
+            print("response when409:",json.loads(response.text) )
+            existing_subscription_id = None
+            try:
+                response_json = json.loads(response.text)
+                if 'id' in response_json:
+                    existing_subscription_id = response_json['id']
+            except (KeyError, ValueError, TypeError):
+                pass
+            print(f"A subscription already exists with subscriptionId: {existing_subscription_id}")
         else:
             print(f"Failed to renew subscription. Status code: {response.status_code}. Message: {response.text}")
     else:
         print("Failed to acquire token.")
 
     # Schedule the next renewal
-    threading.Timer(60 * 60, renew_subscription).start()
+    threading.Timer(600, renew_subscription).start()
 
 # Start the first renewal
 renew_subscription()
--- teamspresencechange.py
+++ teamspresencechange.py
@@ -1,104 +1,73 @@
 import requests
-import webbrowser
-import logging
-import msal
 import json
-from azure.identity.aio import ClientSecretCredential
-from msgraph import GraphServiceClient
-from msgraph.generated.subscriptions.subscriptions_request_builder import SubscriptionsRequestBuilder
-import asyncio
-from msgraph import GraphRequestAdapter
-
-from msgraph.generated.models.subscription import Subscription
+import os
+import threading
+import logging  # Import the logging module
+from azure.identity import InteractiveBrowserCredential
 from datetime import datetime, timedelta
 
-
-
-
-
-logging.basicConfig(level=logging.INFO)
-
 # Azure AD App credentials
-client_id = '4013e8bf-7904-421c-8c8b-5f32441edff6'
-tenant_id = '1f97ae5f-b765-4494-a8a4-5b0955d6673a'
-authority = f'https://login.microsoftonline.com/{tenant_id}'
-client_secret ='5xs8Q~kJUuMVGpKmYib5NfMNA_iYqIcObrIOrapw'
-scopes = ['https://graph.microsoft.com/.default']
+client_id = '09be529a-eedd-46f5-8af4-be2d7332c552'
+tenant_id = '72f988bf-86f1-41af-91ab-2d7cd011db47'
 
 # MS Teams user id
-user_id = '1ecbd423-b0cc-4955-925a-589859e8d58b'
+user_id = '6e65cc58-12fb-4c0d-befa-147fe89270c9'
 
-app = msal.PublicClientApplication(
-    client_id, authority=authority,
-)
-result = None
-
-accounts = app.get_accounts()
-if accounts:
-    result = app.acquire_token_silent(["Presence.Read.All"], account=accounts[0])
-
-if not result:
-    flow = app.initiate_device_flow(scopes=["Presence.Read.All"])
-    if "user_code" not in flow:
-        raise ValueError(
-            "Fail to create device flow. Err: %s" % json.dumps(flow, indent=4))
-
-    print(flow["message"])
-    webbrowser.open(flow["verification_uri"])  # Open the verification_uri in the default web browser
-    result = app.acquire_token_by_device_flow(flow)  # By default, it will block until the user completes the flow
-
-credential = ClientSecretCredential(
-    tenant_id=tenant_id,
-    client_id=client_id,
-    client_secret=client_secret)
+# Create an instance of InteractiveBrowserCredential
+credential = InteractiveBrowserCredential(client_id=client_id, tenant_id=tenant_id, redirect_uri='http://localhost:8000')
 
 
-request_adapter = GraphRequestAdapter(auth_provider=authority)
-graph_client = GraphServiceClient(credential, scopes)
 
-if "access_token" in result:
-    access_token = result['access_token']
-    logging.info(f"Access token retrieved: {access_token}")
-    
-    # Define the subscription payload
-    
-    
-expiration_time = datetime.utcnow() + timedelta(minutes=58)
-expiration_time_str = expiration_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
-    
-async def create_subscription():
+def renew_subscription():
+    # Get a token for the Graph API scope
+    token = credential.get_token('Presence.Read.All')
+    expiration_time = datetime.utcnow() + timedelta(minutes=9)
+    expiration_time_str = expiration_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-        
-        subscription_data = Subscription(
-        change_type = "updated",
-        notification_url = "https://wildteamspresence1.azurewebsites.net/api/presenceNotification?code=umn1zuc1CkeJutJCf1qoK5J0MdfBxx1obJjTM0zC8s80AzFuSxPtEQ==",
-        resource =  f"/communications/presences/{user_id}",
-        expiration_date_time = "2023-09-13T12:30:45",
-        #client_state = None,
-       
-)
-        
-        subscription_data.expirationDateTime = expiration_time_str 
-        
-        try:
-            
-            
-            
-            # Create the subscription using the GraphServiceClient
-            logging.info("Creating the subscription...")
-            
+    if token:
+        # Call a protected API with the access token.
+        headers = {'Authorization': 'Bearer ' + token.token}
 
-            created_subscription = await graph_client.subscriptions.post(body= subscription_data)
-            print("Subscription to presence changes created successfully.")
-        except requests.exceptions.HTTPError as http_err:
-            logging.error(f"HTTP error occurred: {http_err}")
-        except Exception as e:
-            logging.error(f"An error occurred: {str(e)}")
+        # Create a subscription to presence changes
+        subscription_data = {
+            "changeType": "updated",
+            "notificationUrl": "https://wildteamspresence1.azurewebsites.net/api/webhookMSteamsPresenceChange?code=B6v6velmdkfZteTEe2R50tMbiexjlfYG7FRFH9wQuFPSAzFuA9NPgQ==",
+            "resource": f"/communications/presences/{user_id}",
+            "expirationDateTime": expiration_time_str,
+        }
+
+        response = requests.post(
+            'https://graph.microsoft.com/beta/subscriptions',
+            headers=headers,
+            json=subscription_data
+        )
+
+        # Log the JSON response
+        logging.info(f"Response JSON: {response.text}")
+
+        if response.status_code == 201:
+            print("Subscription renewed successfully.")
+        elif response.status_code == 409:    #####This does not work so far
+            # Extract and print the subscriptionId from the response
+            print("response when409:",json.loads(response.text) )
+            existing_subscription_id = None
+            try:
+                response_json = json.loads(response.text)
+                if 'id' in response_json:
+                    existing_subscription_id = response_json['id']
+            except (KeyError, ValueError, TypeError):
+                pass
+            print(f"A subscription already exists with subscriptionId: {existing_subscription_id}")
         else:
-            print(result.get("error"))
-            print(result.get("error_description"))
-            print(result.get("correlation_id"))  # You might need this when reporting a bug.
-if __name__=="__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(create_subscription())
+            print(f"Failed to renew subscription. Status code: {response.status_code}. Message: {response.text}")
+    else:
+        print("Failed to acquire token.")
+
+    # Schedule the next renewal
+    threading.Timer(600, renew_subscription).start()
+
+# Start the first renewal
+renew_subscription()
