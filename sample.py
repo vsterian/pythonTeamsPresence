c41a3936b824749b774bfcdb112d5a8add71089a
Vlad Sterian <vlad.sterian02@gmail.com>
2023-09-14 12:06:50 +0300

    added renewal

--- sample.py
+++ sample.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import os
+import threading
 from azure.identity import InteractiveBrowserCredential
+from datetime import datetime, timedelta
 
 # Azure AD App credentials
 client_id = '09be529a-eedd-46f5-8af4-be2d7332c552'
 tenant_id = '72f988bf-86f1-41af-91ab-2d7cd011db47'
-
 
 # MS Teams user id
 user_id = '6e65cc58-12fb-4c0d-befa-147fe89270c9'
@@ -14,30 +15,43 @@ user_id = '6e65cc58-12fb-4c0d-befa-147fe89270c9'
 # Create an instance of InteractiveBrowserCredential
 credential = InteractiveBrowserCredential(client_id=client_id, tenant_id=tenant_id, redirect_uri='http://localhost:8000')
 
-# Get a token for the Graph API scope
-token = credential.get_token('Presence.Read.All')
+expiration_time = datetime.utcnow() + timedelta(minutes=58)
+expiration_time_str = expiration_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-if token:
-    # Call a protected API with the access token.
-    headers = {'Authorization': 'Bearer ' + token.token}
+def renew_subscription():
+    # Get a token for the Graph API scope
+    token = credential.get_token('Presence.Read.All')
 
-    # Create a subscription to presence changes
-    subscription_data = {
-        "changeType": "updated",
-        "notificationUrl": "https://wildteamspresence1.azurewebsites.net/api/presenceNotification?code=umn1zuc1CkeJutJCf1qoK5J0MdfBxx1obJjTM0zC8s80AzFuSxPtEQ==",
-        "resource": f"/communications/presences/{user_id}",
-        "expirationDateTime":"2023-09-13T15:23:45.9356913Z",
-    }
+    if token:
+        # Call a protected API with the access token.
+        headers = {'Authorization': 'Bearer ' + token.token}
 
-    response = requests.post(
-        'https://graph.microsoft.com/beta/subscriptions',
-        headers=headers,
-        json=subscription_data
-    )
+        # Create a subscription to presence changes
+        subscription_data = {
+            "changeType": "updated",
+            "notificationUrl": "https://wildteamspresence1.azurewebsites.net/api/presenceNotification?code=umn1zuc1CkeJutJCf1qoK5J0MdfBxx1obJjTM0zC8s80AzFuSxPtEQ==",
+            "resource": f"/communications/presences/{user_id}",
+            "expirationDateTime":expiration_time_str,
+        }
 
-    if response.status_code == 201:
-        print("Subscription created successfully.")
+        response = requests.post(
+            'https://graph.microsoft.com/beta/subscriptions',
+            headers=headers,
+            json=subscription_data
+        )
+
+        if response.status_code == 201:
+            print("Subscription renewed successfully.")
+        elif response.status_code == 409:
+            print("A subscription already exists. It must be deleted before another can be made.")
+
+        else:
+            print(f"Failed to renew subscription. Status code: {response.status_code}. Message: {response.text}")
     else:
-        print(f"Failed to create subscription. Status code: {response.status_code}. Message: {response.text}")
-else:
-    print("Failed to acquire token.")
+        print("Failed to acquire token.")
+
+    # Schedule the next renewal
+    threading.Timer(60 * 60, renew_subscription).start()
+
+# Start the first renewal
+renew_subscription()
